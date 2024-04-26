20f3b4095953c4c6379372006accc3624880a185
akshay <akshay@peartree.to>
2024-04-26 21:08:48 +0530

    add inference

--- train.py
+++ train.py
@@ -4,21 +4,12 @@ import torch
 from torch.utils.data import Dataset
 from torch.utils.data.dataloader import DataLoader
 from mingpt.utils import set_seed
+import pickle
+import os
+
 set_seed(3407)
 
-
-import pickle
-
 class SortDataset(Dataset):
-    """ 
-    Dataset for the Sort problem. E.g. for problem length 6:
-    Input: 0 0 2 1 0 1 -> Output: 0 0 0 1 1 2
-    Which will feed into the transformer concatenated as:
-    input:  0 0 2 1 0 1 0 0 0 1 1
-    output: I I I I I 0 0 0 1 1 2
-    where I is "ignore", as the transformer is reading the input sequence
-    """
-
     def __init__(self, split, length=6, num_digits=3):
         assert split in {'train', 'test'}
         self.split = split
@@ -26,61 +17,58 @@ class SortDataset(Dataset):
         self.num_digits = num_digits
     
     def __len__(self):
-        return 10000 # ...
+        return 10000 
     
     def get_vocab_size(self):
         return self.num_digits
     
     def get_block_size(self):
-        # the length of the sequence that will feed into transformer, 
-        # containing concatenated input and the output, but -1 because
-        # the transformer starts making predictions at the last input element
         return self.length * 2 - 1
 
     def __getitem__(self, idx):
-        
-        # use rejection sampling to generate an input example from the desired split
         while True:
-            # generate some random integers
             inp = torch.randint(self.num_digits, size=(self.length,), dtype=torch.long)
-            # half of the time let's try to boost the number of examples that 
-            # have a large number of repeats, as this is what the model seems to struggle
-            # with later in training, and they are kind of rate
-            if torch.rand(1).item() < 0.5:
-                if inp.unique().nelement() > self.length // 2:
-                    # too many unqiue digits, re-sample
-                    continue
-            # figure out if this generated example is train or test based on its hash
+            if torch.rand(1).item() < 0.5 and inp.unique().nelement() > self.length // 2:
+                continue
             h = hash(pickle.dumps(inp.tolist()))
-            inp_split = 'test' if h % 4 == 0 else 'train' # designate 25% of examples as test
+            inp_split = 'test' if h % 4 == 0 else 'train'
             if inp_split == self.split:
-                break # ok
-        
-        # solve the task: i.e. sort
+                break
         sol = torch.sort(inp)[0]
-
-        # concatenate the problem specification and the solution
         cat = torch.cat((inp, sol), dim=0)
-
-        # the inputs to the transformer will be the offset sequence
         x = cat[:-1].clone()
         y = cat[1:].clone()
-        # we only want to predict at output locations, mask out the loss at the input locations
         y[:self.length-1] = -1
         return x, y
 
-
 model_config = GPT.get_default_config()
 model_config.model_type = 'gpt-nano'
-model_config.vocab_size = 50257 # openai's model vocabulary
-model_config.block_size = 1024  # openai's model block_size (i.e. input context length)
+model_config.vocab_size = 3 # Based on num_digits
+model_config.block_size = 11 # Based on length * 2 - 1 for length=6
 model = GPT(model_config)
 train_dataset = SortDataset('train')
 
-
 train_config = Trainer.get_default_config()
-train_config.learning_rate = 5e-4 # many possible options, see the file
+train_config.learning_rate = 5e-4
 train_config.max_iters = 1000
 train_config.batch_size = 32
 trainer = Trainer(train_config, model, train_dataset)
 trainer.run()
+
+# Save the model
+model_path = "sort_model.pth"
+torch.save(model.state_dict(), model_path)
+print("Model saved to", model_path)
+
+# Inference function
+def infer(model, input_sequence):
+    model.eval()
+    with torch.no_grad():
+        input_tensor = torch.tensor(input_sequence, dtype=torch.long).unsqueeze(0) # Batch size of 1
+        output = model(input_tensor)
+    return output.squeeze(0).argmax(dim=-1).tolist() # Simplified prediction, adjust as needed
+
+# Example of inference
+test_input = [2, 0, 1, 1, 0, 2]
+print("Input sequence:", test_input)
+print("Predicted sorted sequence:", infer(model, test_input))
